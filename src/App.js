4bd9f0941820de8e9f19a451ce80c9982983041f
atere21 <tosinateret@gmail.com>
2023-02-02 13:49:27 -0600

    task

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
 import Food from "./components/Food";
-
+import Category from "./components/Category";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Hero/>
        <HeadlineCards/>
        <Food/>
+       <Category/>
     </div>
   );
 }
